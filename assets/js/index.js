320742ee3d3798ecb0838f707ad73d8232a77257
DewaGedeEkaBudhiarta <dodehebat86@gmail.com>
2025-06-30 12:52:31 +0800

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function imageSwitcher(imageUrl) {
+  document.querySelector(".starbucks-image").src = imageUrl;
+}
+
+function changeCircleColor(color) {
+  document.querySelector(".circle").style.background = color;
+}
